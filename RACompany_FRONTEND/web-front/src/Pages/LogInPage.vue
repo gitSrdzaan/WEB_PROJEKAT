37d8014987961b20e59e31cf31c6c0a7c25ad98a
panta246 <stefan.pantic@live.com>
2020-07-26 18:52:51 +0200

    log in i registrovanje forme

--- RACompany_FRONTEND/web-front/src/Pages/LogInPage.vue
+++ RACompany_FRONTEND/web-front/src/Pages/LogInPage.vue
@@ -1,17 +1,68 @@
 <template>
-  <div>
-      <h1>Log In Page</h1>
+  <div class="position">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group 
+        id="input-group-1"
+        label="Password:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.pasword"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-2" 
+        label="Your Username:" 
+        label-for="input-2"
+        >
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter username"
+        ></b-form-input>
+      </b-form-group>
+
+       <b-button type="submit" variant="primary">Log In</b-button>
+      
+    </b-form>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'LogInPage',
-  
-}
+  export default {
+    data() {
+      return {
+        form: {
+          password: '',
+          name: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      }
+    }
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.position{
+  width: 25%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 80px;
+  
+}
 
 </style>
--- RACompany_FRONTEND/web-front/src/Pages/RegisterPage.vue
+++ RACompany_FRONTEND/web-front/src/Pages/RegisterPage.vue
@@ -1,17 +1,119 @@
 <template>
-  <div>
-      <h1>Register Page</h1>
+  <div class="position">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group 
+        id="input-group-1"
+        label="Password:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.pasword"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-2" 
+        label="Your Username:" 
+        label-for="input-2"
+        >
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter username"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-3" 
+        label="Your firstname:" 
+        label-for="input-2"
+        >
+        <b-form-input
+          id="input-2"
+          v-model="form.firstname"
+          required
+          placeholder="Enter firstname"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group 
+        id="input-group-3" 
+        label="Your lastname:" 
+        label-for="input-2"
+        >
+        <b-form-input
+          id="input-2"
+          v-model="form.lastname"
+          required
+          placeholder="Enter lastname"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Sex:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.sex"
+          :options="sexes"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Role:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.role"
+          :options="roles"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Sign Up</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'RegisterPage',
-  
-}
+  export default {
+    data() {
+      return {
+        form: {
+          password: '',
+          name: '',
+        },
+        sexes: [
+          'Male',
+          'Female'
+        ],
+        roles: [
+          'GUEST',
+          'HOST'
+        ],
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      }
+    }
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.position{
+  width: 25%;
+  height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 80px;
+  
+}
 
 </style>
