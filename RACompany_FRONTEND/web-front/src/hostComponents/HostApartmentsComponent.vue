16a723fd8d11db0e61d478d15b094dcd23bfb52e
gitSrdzaan <srdjan_kralj_97@live.com>
2020-08-04 16:40:44 +0200

    modal za izmjenu apartmana

--- RACompany_FRONTEND/web-front/src/hostComponents/HostApartmentsComponent.vue
+++ RACompany_FRONTEND/web-front/src/hostComponents/HostApartmentsComponent.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
         <b-list-group v-for="apartment in this.apartments" :key="apartment.id">
-            <b-list-group-item button>{{apartment.id}}</b-list-group-item>
+            <b-list-group-item button v-b-modal.modal >{{apartment.id}}</b-list-group-item>
         </b-list-group>
+
+        <b-modal id="Modal">
+            <p> Modal </p>
+        </b-modal>
     </div>
 </template>
 
