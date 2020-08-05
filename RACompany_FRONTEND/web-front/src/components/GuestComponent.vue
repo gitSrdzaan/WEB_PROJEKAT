d252f04dd797f4679e9034f2ef2703d0af846af4
gitSrdzaan <srdjan_kralj_97@live.com>
2020-08-05 15:38:15 +0200

    komponenta za personalne informacije gosta kao korisnika

--- RACompany_FRONTEND/web-front/src/components/GuestComponent.vue
+++ RACompany_FRONTEND/web-front/src/components/GuestComponent.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
       <h1>Guest Component</h1>
+      <div>
+        <b-tabs>
+          <b-tab title="Personal info">
+            <UserInfoComponent v-bind:user="user"/>
+          </b-tab>
+          <b-tab title="Reservations">
+            <p>Yours reservations</p>
+          </b-tab>
+        </b-tabs>
+      </div>
   </div>
 </template>
 
 <script>
+import UserInfoComponent from "../components/UserInfoComponent"
 export default {
   name: 'GuestComponent',
+  components :{
+    UserInfoComponent
+  },
+  data(){
+    return {
+      user : {}
+    }
+  }
   
 }
 </script>
