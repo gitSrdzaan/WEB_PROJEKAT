5439483cd322afedfe66abf0abc69f4b08547a7d
gitSrdzaan <srdjan_kralj_97@live.com>
2020-08-30 18:37:14 +0200

    Servis za rezervaciju

--- RACompany/src/services/ReservationService.java
+++ RACompany/src/services/ReservationService.java
@@ -3,14 +3,21 @@ package services;
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import beans.Reservation;
 import dao.ReservationDAO;
 
-@Path("/reservations")
+@Path("/reservation")
 public class ReservationService {
 
 	@Context
@@ -30,8 +37,75 @@ public class ReservationService {
 	
 	@GET
 	@Path("/getAll")
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAll(@Context HttpServletRequest request) {
 		ReservationDAO dao = (ReservationDAO) this.ctx.getAttribute("reservationDAO");
 		return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(dao.findAll()).build();
 	}
+	
+	@GET
+	@Path("/get/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getReservation (@PathParam("id") Long id, @Context HttpServletRequest request) {
+		
+		ReservationDAO dao = (ReservationDAO) this.ctx.getAttribute("reservationDAO");
+		
+		Reservation reservation = dao.findById(id);
+		if(reservation == null) {
+			return Response.status(400).entity("GRESKA: Rezervacija ne postoji").header("Allow-Control-Allow-Origin", "*").build();
+		}
+		
+		return Response.status(200).entity(reservation).header("Allow-Control-Allow-Origin", "*").build();
+
+		
+		
+	}
+	
+	@POST
+	@Path("/new")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createReservation(Reservation reservation,@Context HttpServletRequest request) {
+		ReservationDAO dao = (ReservationDAO) this.ctx.getAttribute("reservationDAO");
+		
+		dao.putReservation(reservation);
+		
+		try {
+			dao.saveReservation();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(500).header("Access-Control-Allow-Origin", "*").build();
+		}
+		
+		
+		
+		return Response.status(200).header("Access-Control-Allow-Origin", "*").build();
+	}
+	
+	@PUT
+	@Path("/modify/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response modifyReservation(Reservation reservation,@PathParam("id") Long id
+			,@Context HttpServletRequest request) {
+		ReservationDAO dao = (ReservationDAO) this.ctx.getAttribute("reservationDAO");
+		dao.modifyReservation(reservation,id);
+		
+		try {
+			dao.saveReservation();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(500).entity("Greska pri modifikaciji rezervacije").build();
+		}
+
+		
+		
+		
+		return Response.ok().build();
+	
+	}
+	
+	
+	
+	
 }
