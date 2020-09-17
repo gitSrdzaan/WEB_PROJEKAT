6995b192c15ea97cbabd96363d51cedcde5790b6
gitSrdzaan <srdjan_kralj_97@live.com>
2020-09-17 21:11:08 +0200

    pregled rezervacija kod domacina i gosta

--- RACompany_FRONTEND/web-front/src/components/ReservationComponent.vue
+++ RACompany_FRONTEND/web-front/src/components/ReservationComponent.vue
@@ -2,25 +2,25 @@
     <div>
         <b-form>
             <b-form-group id="input-aprtment-id" label="Apartment id" label-for="input-reservation-apartment">
-                <b-form-input id="input-reservation-apartment" v-model="reservation.reservedAppatment.id" disabled />
+                <b-form-input id="input-reservation-apartment" v-model="reservation.reservedAppatment.id" :disabled="role === 'ADMIN'" />
             </b-form-group>
             <b-form-group id="input-guest-username" label="Guest username" label-for="input-reservation-guest">
-                <b-form-input id="input-reservation-guest" v-model="reservation.guest.username" disabled />
+                <b-form-input id="input-reservation-guest" v-model="reservation.guest.username" :disabled="role === 'ADMIN'" />
             </b-form-group>
             <b-form-group id="input-group-startDate" label="Start date" label-for="input-reservation-startdate">
-                <b-form-datepicker id="input-reservation-startdate" v-model="startDate" disabled />
+                <b-form-datepicker id="input-reservation-startdate" v-model="startDate" :disabled="role === 'ADMIN'"  />
             </b-form-group>
             <b-form-group id="input-group-nightNumber" label="Number of nights" label-for="input-reservation-nightNumber">
-                <b-form-input id="input-reservation-nightNumber" v-model="reservation.nightNumber" disabled />
+                <b-form-input id="input-reservation-nightNumber" v-model="reservation.nightNumber" :disabled="role === 'ADMIN'" />
             </b-form-group>
             <b-form-group id="input-group-price" label="Final price" label-for="input-reservation-price">
-                <b-form-input id="input-reservation-price" v-model="reservation.price" disabled />
+                <b-form-input id="input-reservation-price" v-model="reservation.price" :disabled="role === 'ADMIN'" />
             </b-form-group>
             <b-form-group id="input-group-message" label="Message" label-for="input-reservation-message">
-                <b-form-input id="input-reservation-message" v-model="reservation.message" disabled />
+                <b-form-input id="input-reservation-message" v-model="reservation.message" :disabled="role === 'ADMIN'" />
             </b-form-group>
             <b-form-group id="input-group-status" label="Reservation status" label-for="input-reservation-status">
-                <b-form-input id="input-reservation-status" v-model="reservation.status" disabled />
+                <b-form-input id="input-reservation-status" v-model="reservation.status" :disabled="role === 'ADMIN'"/>
             </b-form-group>
             
         </b-form>
@@ -33,6 +33,9 @@ export default {
     props : {
         reservation  : {
             type : Object
+        },
+        role : {
+            type : String
         }
     },
     data(){
